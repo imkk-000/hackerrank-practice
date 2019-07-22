a1b96302c997d8986756bf73fb494df1b413999b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-22 15:31:37 +0700

    Edit: redefine name of import module of day 0

--- 30_days_of_code/day-0-hello-world/main_test.go
+++ 30_days_of_code/day-0-hello-world/main_test.go
@@ -1,14 +1,14 @@
 package main_test
 
 import (
-	dojo "dojo"
+	main "dojo"
 	"testing"
 )
 
 func TestShowHelloInputHello30DaysOfCodeShouldReturnHelloWorldWithHello30DaysOfCode(t *testing.T) {
 	expectedResult := "Hello, World.\nWelcome to 30 Days of Code!"
 
-	actualResult := dojo.ShowHello("Welcome to 30 Days of Code!")
+	actualResult := main.ShowHello("Welcome to 30 Days of Code!")
 
 	if expectedResult != actualResult {
 		t.Errorf("expect '%s' but it got '%s'", expectedResult, actualResult)
